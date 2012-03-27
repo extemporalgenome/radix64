493d69462121da9df8dd6d1a6953391585238ba6
Kevin Gillette <extemporalgenome@gmail.com>
2012-03-27 15:06:38 -0600

    Replaced JS decode if/else chain with a table lookup implementation; may or may not be faster, but is certainly much simpler.

--- radix64.js
+++ radix64.js
@@ -4,12 +4,12 @@
 
 var enc64, dec64;
 (function() {
-	// base64url
+	// modified base64url
 	var ord = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_";
-	var dmin = '0'.charCodeAt(0), dmax = '9'.charCodeAt(0);
-	var umin = 'A'.charCodeAt(0), umax = 'Z'.charCodeAt(0), uoff = ord.indexOf('A');
-	var lmin = 'a'.charCodeAt(0), lmax = 'z'.charCodeAt(0), loff = ord.indexOf('a');
-	var c62 = ord.charCodeAt(62), c63 = ord.charCodeAt(63);
+	var table = new Array(256);
+	for(var i = 0; i < ord.length; i++)
+		table[ord.charCodeAt(i)] = i;
+
 	enc64 = function(n, len) {
 		var out = []; 
 		while(n > 0 || len > 0) {
@@ -22,18 +22,8 @@ var enc64, dec64;
 	dec64 = function(s) {
 		var c, out = 0; 
 		for(var i = 0; i < s.length; i++) {
-			c = s.charCodeAt(i);
-			if(c >= dmin && c <= dmax)
-				c -= dmin;
-			else if(c >= umin && c <= umax)
-				c = c - umin + uoff;
-			else if(c >= lmin && c <= lmax)
-				c = c - lmin + loff;
-			else if(c == c62)
-				c = 62;
-			else if(c == c63)
-				c = 63;
-			else
+			c = table[s.charCodeAt(i)];
+			if(c === undefined)
 				return -1;
 			out = (out << 6) | c;
 		}
